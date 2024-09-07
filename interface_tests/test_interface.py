da1b6b966a069b98d2c6f1d2e32d2d5f0b14be27
Leviathan-CE <leviathancreative@outlook.com>
2024-09-07 14:03:18 -0700

    minor adjustments

--- interface_tests/test_interface.py
+++ interface_tests/test_interface.py
@@ -1,8 +1,31 @@
 
 from InterfaceTools.interface import InterfaceInstancingError, interface
-from interface_tests.testclasses import My2Class, MyClass, Myinter
-import pytest
 
+
+class yolo:
+    
+    def yolo2(self):pass
+
+@interface
+class MyClass():
+   
+   
+   def method_1(self): 
+            pass
+@interface    
+class Myinter():
+    
+    def method_3(self):pass
+
+class My2Class(MyClass, yolo):
+    
+    def __init__(self):
+        pass
+    
+    def method_2(self):
+        pass
+
+    
 
 
 

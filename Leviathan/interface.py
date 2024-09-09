a1dd9204f624e35e6b0b391c2a3147509b98f24a
Leviathan-CE <leviathancreative@outlook.com>
2024-09-09 13:05:22 -0700

    removed types import

--- Leviathan/interface.py
+++ Leviathan/interface.py
@@ -1,4 +1,4 @@
-import types
+
 
 
 class InterfaceInstancingError(Exception):
@@ -31,7 +31,7 @@ def Interface(cls:type) -> type:
             return object.__new__(cls,*args,**kwargs)     
    
    
-    jclass = types.new_class(cls.__name__, (cls,metaInterface), cls.__annotations__) 
+    jclass = type(cls.__name__, (cls,metaInterface), cls.__annotations__) 
     return jclass
 
 
